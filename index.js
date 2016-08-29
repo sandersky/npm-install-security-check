a83cbfa74a64c4bdaa82ba47afd9b3fa09d4a026
Matthew Dahl <dahl.matthew@gmail.com>
2016-08-29 16:26:59 -0700

    Use node script instead of bash

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+
+var warn = require('cli-color').yellow
+
+var msg = 'Warning: You are running "npm install" with scripts enabled which is a potential security risk.\n' +
+          'You should run npm install with the --ignore-scripts flag or update your npm config via:\n\n' +
+          '  npm config set ignore-scripts true\n\n' +
+          'For more information read http://blog.npmjs.org/post/141702881055/package-install-scripts-vulnerability'
+
+console.log(warn(msg))
--- install-warning.sh
+++
@@ -1,8 +0,0 @@
-echo -e "\033[33m
-Warning: You are running "npm install" with scripts enabled which is a potential security risk.
-You should run npm install with the --ignore-scripts flag or update your npm config via:
-
-  npm config set ignore-scripts true
-
-For more information read http://blog.npmjs.org/post/141702881055/package-install-scripts-vulnerability
-\033[0m"
