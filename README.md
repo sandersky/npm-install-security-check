6a277ee854bfee1d2de41ce9ffda4322130645f4
Matthew Dahl <dahl.matthew@gmail.com>
2016-09-07 16:33:53 -0700

    Make silencable

--- README.md
+++ README.md
@@ -18,3 +18,17 @@ console if they don't have scripts turned off on npm install.
 
 [npm-img]: https://img.shields.io/npm/v/npm-install-security-check.svg "NPM Version"
 [npm-url]: https://www.npmjs.com/package/npm-install-security-check
+
+## Silence warning
+
+If you want to silence the warning that this package prints to the console
+simply add the following file to the directory in which you run `npm install`
+from or any of it's parent directories.
+
+*npm-install-security-check.json *
+
+```json
+{
+  "silent": true
+}
+```
--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 #! /usr/bin/env node
 
+var configContents
+var currentDir = process.cwd()
+var fs = require('fs')
+var path = require('path')
+
+// Check for a config file in parent directories until we find one
+while (currentDir.length !== 0) {
+  var configPath = path.join(currentDir, 'npm-install-security-check.json')
+
+  try {
+    configContents = fs.readFileSync(configPath)
+    break // since we found a config file we can stop looking
+  } catch (err) {
+    // do nothing
+  }
+
+  var segments = currentDir.split(path.sep)
+
+  if (segments.length === 1) {
+    currentDir = ''
+  } else {
+    segments.pop()
+    currentDir = segments.join(path.sep)
+  }
+}
+
+if (configContents) {
+  try {
+    var config = JSON.parse(configContents)
+    if (config.silent) {
+      process.exit(0)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 var msg = 'Warning: You are running "npm install" with scripts enabled which is a potential security risk.\n' +
           'You should run npm install with the --ignore-scripts flag or update your npm config via:\n\n' +
           '  npm config set ignore-scripts true\n\n' +
